afe347472280e9b1081ac28ce1082f3c08133b8b
Chaitanya Datar <datar.chaitanya@gmail.com>
2026-02-27 16:47:47 +0000

    Create solution.cs

---
+++ solution.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    private string builder(int n){
+        StringBuilder sb = new StringBuilder();
+        int i = 1;
+        while(n > 0){
+            sb.Append(n % 10);
+            n /= 10;
+
+            if(i++ % 3 == 0 && n > 0){
+                sb.Append('.');
+            }
+        }
+
+        return(new string(sb.ToString().Reverse().ToArray()));
+    }
+
+    public string ThousandSeparator(int n) {
+        if(n <= 999){
+            return("" + n);
+        }
+
+        return(builder(n));
+    }
+}
